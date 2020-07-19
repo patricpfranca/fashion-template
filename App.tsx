ca7774546d541f1fcffdefb93322fd436bba36b0
Patric Pereira <patric.pereirafranca@gmail.com>
2020-07-19 11:20:44 -0300

    refactoring routes authentication

--- App.tsx
+++ App.tsx
@@ -1,31 +1,17 @@
 import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import { ThemeProvider } from '@shopify/restyle';
 
 import {
-  Onboarding,
-  Welcome,
   assets as authenticationAssets,
+  AuthenticationNavigator,
 } from './src/Authentication';
 import { LoadAssets, theme } from './src/components';
-import { Routes } from './src/components/Navigation';
 
 const assets = [...authenticationAssets];
 const fonts = {
   'SFProText-Bold': require('./assets/fonts/SF-Pro-Text-Bold.otf'),
   'SFProText-Semibold': require('./assets/fonts/SF-Pro-Text-Semibold.otf'),
   'SFProText-Regular': require('./assets/fonts/SF-Pro-Text-Regular.otf'),
-};
-
-const AuthenticationStack = createStackNavigator<Routes>();
-
-const AuthenticationNavigator = () => {
-  return (
-    <AuthenticationStack.Navigator headerMode="none">
-      <AuthenticationStack.Screen name="OnBoarding" component={Onboarding} />
-      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
-    </AuthenticationStack.Navigator>
-  );
 };
 
 export default function App() {
--- src/Authentication/index.tsx
+++ src/Authentication/index.tsx
@@ -1,6 +1,20 @@
-import { assets as onBoardingAssets } from './Onboarding';
-import { assets as welcomeAssets } from './Welcome';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Routes } from '../components/Navigation';
+import Onboarding, { assets as onBoardingAssets } from './Onboarding';
+import Welcome, { assets as welcomeAssets } from './Welcome';
 
 export { default as Onboarding } from './Onboarding';
 export { default as Welcome } from './Welcome';
 export const assets = [...onBoardingAssets, ...welcomeAssets];
+
+const AuthenticationStack = createStackNavigator<Routes>();
+export const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="OnBoarding" component={Onboarding} />
+      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
+    </AuthenticationStack.Navigator>
+  );
+};
